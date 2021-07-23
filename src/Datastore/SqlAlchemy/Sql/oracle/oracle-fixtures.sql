a2aa6d5c26dd3ae44e641d7a032b2fd51f66ddb8
Martin Abbrent <martin.abbrent@ufz.de>
2021-07-23 11:54:41 +0200

    Split Sqlalchemy ddl and fixtures into oracle and postgres subdirs

---
+++ src/Datastore/SqlAlchemy/Sql/oracle/oracle-fixtures.sql
@@ -0,0 +1,28 @@
+INSERT INTO THING (ID, NAME, UUID, DESCRIPTION, PROPERTIES)
+VALUES (1, 'My Oracle Thing', '7f384bcc-ea5d-11eb-9d12-54e1ad7c5c19', null, '{
+  "parsers": [
+    {
+      "type": "AnotherCustomParser",
+      "settings": {
+        "delimiter": ",",
+        "footlines": 0,
+        "headlines": 1,
+        "timestamp": {
+          "date": {
+            "pattern": "^(\\d{4})-(\\d{2})-(\\d{2})",
+            "position": 1,
+            "replacement": "$1-$2-$3"
+          },
+          "time": {
+            "pattern": "(\\d{2}):(\\d{2}):(\\d{2})$",
+            "position": 1,
+            "replacement": "$1:$2:$3"
+          }
+        }
+      }
+    },
+    {
+      "type": "MyCustomParser"
+    }
+  ]
+}');
