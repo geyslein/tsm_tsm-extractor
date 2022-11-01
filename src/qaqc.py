27bbaff560c56bd6de4cc131abcdab6dc624930d
Bert Palm <bert.palm@ufz.de>
2022-11-01 18:54:18 +0100

    improved fetching data for context_window. added some docstrings

--- src/qaqc.py
+++ src/qaqc.py
@@ -16,11 +16,36 @@ from collections import OrderedDict
 from typing import Dict, Callable, List
 
 import pandas as pd
+from pandas.api.types import is_integer
 import saqc
 from saqc.core.core import DictOfSeries
 
 
 def parse_qaqc_config(datastore):
+    """
+    Fetch the QAQC config from datastore and parse it into a DataFrame.
+
+    Examples
+    --------
+    >>> parse_qaqc_config(datastore)
+                                        kwargs   function  position  window
+    0                   {'max': 100, 'min': 0}  flagRange         0       3
+    1                    {'max': 98, 'min': 2}  flagRange         2       3
+    2  {'window': '60Min', 'min_residuals': 5}    flagMAD         0       3
+
+    Parameters
+    ----------
+    datastore : SqlAlchemyDatastore
+        The datastore to fetch the config from/for.
+
+    Notes
+    -----
+    The window column always have the same value over all rows.
+
+    Returns
+    -------
+    config: pd.DataFrame
+    """
     thing = datastore.sqla_thing
     idx = thing.properties["QAQC"]["default"]
     config = thing.properties["QAQC"]["configs"][idx]
@@ -33,15 +58,11 @@ def parse_qaqc_config(datastore):
     config_df = pd.DataFrame(config["tests"])
     config_df["window"] = parse_window(config["context_window"])
 
-    # example `config_df`
-    #                                     kwargs   function  position          window
-    # 0                   {'max': 100, 'min': 0}  flagRange         0 0 days 01:00:00
-    # 1                    {'max': 98, 'min': 2}  flagRange         2 0 days 01:00:00
-    # 2  {'window': '60Min', 'min_residuals': 5}    flagMAD         0 0 days 01:00:00
     return config_df
 
 
 def parse_window(window) -> pd.Timedelta | int:
+    """Parse the `context_window` value of the config."""
     if isinstance(window, int) or isinstance(window, str) and window.isnumeric():
         window = int(window)
         is_negative = window < 0
@@ -58,6 +79,33 @@ def get_datastream_data(
     datastream: Datastream,
     window: int | pd.Timedelta,
 ) -> pd.DataFrame | None:
+    """
+    Get data that has no quality flags yet.
+
+    Parameters
+    ----------
+    datastore : SqlAlchemyDatastore
+        Datastore to use.
+
+    datastream : Datastream
+        Datastream to fetch the data from
+
+    window : int or pandas.Timedelta
+        The context window of data to fetch additionally.
+
+        If an integer, it defines the number of preceding observations to
+        fetch additionally, before the first unprocessed observation.
+
+        If a Timedelta, it defines an offset-window to fetch additionally
+        before the timestamp of the first unprocessed observation.
+
+        The first unprocessed observation is the earliest observation
+        which never was quality-controlled before.
+
+    Returns
+    -------
+    data: pd.DataFrame or None
+    """
     query = datastore.session.query(Observation.result_time).filter(
         Observation.datastream == datastream,
         Observation.result_quality == sqlalchemy.JSON.NULL,
@@ -82,7 +130,7 @@ def get_datastream_data(
         Observation.result_time <= more_recent,
         Observation.result_time >= less_recent,
     )
-    main_data = pd.read_sql(
+    main_data: pd.DataFrame = pd.read_sql(
         query.statement,
         query.session.bind,
         parse_dates=True,
@@ -93,52 +141,30 @@ def get_datastream_data(
         return None
 
     # numeric window
-    # --------------
-    # get window-many additional observations
-    # preceding the first observation of the new data.
-    # the window define a number of observations e.g. `500`
-    if isinstance(window, int):
-
-        query = base_query.filter(
-            Observation.result_time < less_recent,
-        ).order_by(sqlalchemy.desc(Observation.result_time))
-
-        chunks = pd.read_sql(
-            query.statement,
-            query.session.bind,
-            parse_dates=True,
-            index_col="result_time",
-            chunksize=window,
+    if is_integer(window):  # detect numpy.int64
+        query = (
+            base_query.filter(
+                Observation.result_time < less_recent,
+            )
+            .order_by(sqlalchemy.desc(Observation.result_time))
+            .limit(window)
         )
-        try:
-            window_data = next(chunks)
-        except StopIteration:  # no data
-            window_data = pd.DataFrame([])
 
     # datetime window
-    # ---------------
-    # get arbitrary many observations, which lies in the
-    # window before the first observation. The window is
-    # defined by a datetime-offset e.g. `'7d'`
     else:
-        window: pd.Timedelta
         query = base_query.filter(
             Observation.result_time < less_recent,
             Observation.result_time >= less_recent - window,
         )
-        window_data = pd.read_sql(
-            query.statement,
-            query.session.bind,
-            parse_dates=True,
-            index_col="result_time",
-        )
 
-    if window_data.empty:
-        data = main_data
-    else:
-        data = pd.concat([main_data, window_data], sort=True)
+    window_data: pd.DataFrame = pd.read_sql(
+        query.statement,
+        query.session.bind,
+        parse_dates=True,
+        index_col="result_time",
+    )
 
-    return data
+    return pd.concat([main_data, window_data], sort=True, copy=False)
 
 
 def get_data(datastore: SqlAlchemyDatastore, config: pd.DataFrame):
@@ -178,18 +204,38 @@ def get_data(datastore: SqlAlchemyDatastore, config: pd.DataFrame):
 
 
 def run_qaqc_config(data: saqc.SaQC, config: pd.DataFrame):
+    """
+    Run a qc-tests from config on given data.
+
+    Parameters
+    ----------
+    data : saqc.SaQC
+        Hold data and quality labels (aka. flags).
+
+    config : pd.dataFrame
+        Collection of tests to run on data.
+
+    Returns
+    -------
+    processed : saqc.SaQC
+        Hold data and quality labels (aka. flags).
+    """
     for idx, row in config.iterrows():
-        # var is position for now, until SMS is inplace
+        # We use the position as variable name
+        # for now, until SMS is inplace and can
+        # provide better naming.
         var = str(row["position"])
         func = row["function"]
         kwargs = row["kwargs"]
         info = config.loc[idx].to_dict()
-        data = run_saqc_funtion(data, var, func, kwargs, info)
+        data = _run_saqc_funtion(data, var, func, kwargs, info)
 
     return data
 
 
-def run_saqc_funtion(qc_obj, var_name, func_name, kwargs, info):
+def _run_saqc_funtion(
+    qc_obj: saqc.SaQC, var_name: str, func_name: str, kwargs: dict, info: dict
+):
     method = getattr(qc_obj, func_name, None)
     logging.debug(f"running SaQC with {info=}")
     qc_obj = method(var_name, **kwargs)
